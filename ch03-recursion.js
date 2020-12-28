7aa14aac219ba22ee1fc33b7cbc1a7decc1fa5f2
akassabian <alainkassabian@gmail.com>
2020-12-28 10:19:21 -0500

    use Math.abs to DRY refactor recursion exercise

--- ch03-recursion.js
+++ ch03-recursion.js
@@ -1,13 +1,10 @@
 function isEven(number) {
+  number = Math.abs(number);
   if (number === 0) {
     return true;
   } else if (number === 1) {
     return false;
   } else {
-    if (number > 0) {
-      return isEven(number - 2);
-    } else {
-      return isEven(number + 2);
-    }
+    return isEven(number - 2);
   }
 }
