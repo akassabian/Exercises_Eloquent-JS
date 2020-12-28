42006614aad05e78a1dda3d3acabf4c82ba5dc9f
akassabian <alainkassabian@gmail.com>
2020-12-28 10:45:08 -0500

    add step variable to range func

--- ch04-sum-of-a-range.js
+++ ch04-sum-of-a-range.js
@@ -1,13 +1,19 @@
-function range(x, y = null) {
+function range(x, y = null, step = 1) {
   rangeArray = [];
-  if (x === y || y === null) {
+  if (
+    (step === Math.abs(0) && !(x === y || y === null)) ||
+    (x > y && Math.sign(step) === 1) ||
+    (x < y && Math.sign(step) === -1)
+  ) {
+    return "Start value will never equal end value, given current step value (default step is +1).";
+  } else if (x === y || y === null) {
     rangeArray.push(x);
   } else if (x > y) {
-    for (i = y; i <= x; i++) {
-      rangeArray.unshift(i);
+    for (i = x; i >= y; i += step) {
+      rangeArray.push(i);
     }
   } else if (x < y) {
-    for (i = x; i <= y; i++) {
+    for (i = x; i <= y; i += step) {
       rangeArray.push(i);
     }
   }
