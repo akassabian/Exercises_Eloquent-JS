a487edc365ad8d170d022c0775a7f6b0950655eb
akassabian <alainkassabian@gmail.com>
2021-01-09 13:46:52 -0500

    add dominant writing direction exercise

---
+++ ch06-dominant-writing-direction.js
@@ -0,0 +1,42 @@
+function countBy(items, groupName) {
+  let counts = [];
+  for (let item of items) {
+    let name = groupName(item);
+    let known = counts.findIndex((c) => c.name == name);
+    if (known == -1) {
+      counts.push({ name, count: 1 });
+    } else {
+      counts[known].count++;
+    }
+  }
+  return counts;
+}
+
+function characterScript(code) {
+  for (let script of SCRIPTS) {
+    if (
+      script.ranges.some(([from, to]) => {
+        return code >= from && code < to;
+      })
+    ) {
+      return script;
+    }
+  }
+  return null;
+}
+
+function dominantDirection(text) {
+  let scripts = countBy(text, (char) => {
+    let script = characterScript(char.codePointAt(0));
+    return script ? script.direction : "none";
+  }).filter(({ direction }) => direction != "none");
+  console.log(scripts);
+  return scripts.reduce((a, b) => {
+    return a.count < b.count ? b : a;
+  }).name;
+}
+
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection("Hey, مساء الخير"));
+// → rtl
